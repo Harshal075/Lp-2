938656cb874c1cce78bdeb365292993f63768cb3
Harshal075 <96949565+Harshal075@users.noreply.github.com>
2024-05-05 15:30:25 +0530

    Create addition.cpp

---
+++ addition.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  cout << "Hello , My name is Harshal";
+  cout << "\n";
+  cout << "Enter 2 Number: ";
+  int a , b;
+  cin >> a;
+  cin >> b;
+  cout << "Addtiton of 2 number is : " << a + b;
+  return 0;
+}
